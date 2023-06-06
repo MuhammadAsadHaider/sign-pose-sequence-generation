43939da3b1bd5c16547eba5ffff66eefc6972d45
NET\mu979926 <haider24@knights.ucf.edu>
2023-06-05 22:11:49 -0400

    nit

--- feature_extractor.py
+++ feature_extractor.py
@@ -72,13 +72,17 @@ key_points_path = "features/"
 if not os.path.exists(key_points_path):
     os.makedirs(key_points_path)
 vids = os.listdir(vid_folder)
+# order videos
+vids.sort()
 
-vids_start = 2
-vids_end = 2000
+print(vids[:5])
+
+vids_start = 0
+vids_end = 5000
 
 
 for vid in tqdm(vids[vids_start:vids_end]):
-    vid_path = os.path.join(vid_folder, vid.replace('\\', '/'))
+    vid_path = os.path.join(vid_folder, vid).replace("\\", '/')
     try:
         feature_extractor_video(vid_path, key_points_path)
     except:
